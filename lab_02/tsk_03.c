a12bd9c7bbbb6786713fb4f7fbce55de7586ac7d
Chanuth412-pixel <e21007@eng.pdn.ac.lk>
2026-05-20 10:40:47 +0530

    tsk03 and tsk04

--- lab_02/tsk_03.c
+++ lab_02/tsk_03.c
@@ -1,42 +1,34 @@
 #include <avr/io.h>
-#include <avr/interrupt.h>
+#include <avr/interrupt.h> 
 #include <util/delay.h>
 
-volatile uint8_t count = 0;
-volatile uint8_t pin_changed = 0;
+uint8_t ct =0;  
+volatile uint8_t flg=0;
 
-// Set flag when Pin 7 changes state
-ISR(PCINT2_vect) {
-    pin_changed = 1;
+ISR(PCINT2_vect) {   
+    flg =1;
 }
 
 int main(void) {
-    DDRB = 0x3F;          // Pins 8–13 as LED outputs
-    DDRD &= ~(1 << PD7);  // Pin 7 as input
-    PORTD |= (1 << PD7);  // Enable internal pull-up
-
-    PCICR |= (1 << PCIE2);     // Enable Port D pin-change interrupts
-    PCMSK2 |= (1 << PCINT23);  // Enable interrupt on Pin 7
-
-    sei(); // Enable global interrupts
-
-    uint8_t last_state = 1;
+    DDRB =  0x3F;          
+    DDRD &=  ~(1 <<PD7);   
+    PORTD |=  (1<< PD7);  
+    PCICR |= (1<< PCIE2);     
+    PCMSK2 |= (1 <<PCINT23);  
+    sei(); 
+    uint8_t lst=1;
 
     while (1) {
-        if (pin_changed) {
-            _delay_ms(50); // Debounce delay
-
-            uint8_t current_state = (PIND & (1 << PD7)) >> PD7;
-
-            // Count once when the button is released
-            if (last_state == 0 && current_state == 1) {
-                count++;
-                PORTB = count & 0x3F;
+       if (flg) {
+             _delay_ms(50); 
+              uint8_t curr = (PIND & (1 <<PD7))>> PD7;
+             if (lst== 0 && curr==1) {
+                 ct++;
+                PORTB = ct & 0x3F;
             }
-
-            last_state = current_state;
-            PCIFR |= (1 << PCIF2); // Clear interrupt flag
-            pin_changed = 0;
+            lst=curr;
+            PCIFR |= (1 << PCIF2); 
+            flg =0;
         }
     }
 
